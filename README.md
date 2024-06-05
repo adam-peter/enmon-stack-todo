84ba0d53fea06100f6d77e33d23e4b7868688001
adampeter111 <petrzela@enmon.tech>
2024-06-05 10:27:11 +0200

    update readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Enmon-stack Todo App 🟩🟦
+
+This is a super-over-engineered todo app. It's purpouse is to play around with the exact tech stack of Enmon, my new company.
+
+## Stack 🚀
+
+BE:
+  - Nestjs
+  - MongoDB -> Mongoose
+FE:
+  - React, Vite
+  - ReactQuery
+  - StyledComponents
--- backend/src/todos/todos.controller.ts
+++ backend/src/todos/todos.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/CreateTodoDto';
 import { UpdateTodoDto } from './dto/UpdateTodoDto';
